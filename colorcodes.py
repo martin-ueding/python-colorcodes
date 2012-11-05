35f39575fc4be4407d8159b7059f3344645a4760
Martin Ueding <dev@martin-ueding.de>
2012-11-05 20:21:41 +0100

    Init

---
+++ colorcodes.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright © 2012 Martin Ueding <dev@martin-ueding.de>
+
+__docformat__ = "restructuredtext en"
+
+class Colorcodes(object):
+    """
+    Provides ANSI terminal color codes which are gathered via the ``tput``
+    utility. That way, they are portable. If there occurs any error with
+    ``tput``, all codes are initialized as an empty string.
+
+    The provided fields are listed below.
+
+    Control:
+
+    - bold
+    - reset
+
+    Colors:
+
+    - blue
+    - green
+    - orange
+    - red
+
+    :license: MIT
+    """
+    def __init__(self):
+        try:
+            self.bold = subprocess.check_output("tput bold".split())
+            self.reset = subprocess.check_output("tput sgr0".split())
+
+            self.blue = subprocess.check_output("tput setaf 4".split())
+            self.green = subprocess.check_output("tput setaf 2".split())
+            self.orange = subprocess.check_output("tput setaf 3".split())
+            self.red = subprocess.check_output("tput setaf 1".split())
+        except subprocess.CalledProcessError as e:
+            self.bold = ""
+            self.reset = ""
+
+            self.blue = ""
+            self.green = ""
+            self.orange = ""
+            self.red = ""
+
